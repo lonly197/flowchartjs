c5db7c7668f67262e6b72f594d2cc4220546085f
lonly <lonly197@qq.com>
2017-11-13 16:06:20 +0800

    update footer's github link

--- src/components/common/Footer.vue
+++ src/components/common/Footer.vue
@@ -2,7 +2,7 @@
   <el-row>
     <el-col :span="24">
       <h4>A Simple FlowChart Libary Base On JsPlumb</h4>
-      <a href="https://github.com/lonly197/flowchart">Host on GitHub</a>
+      <a href="https://github.com/lonly197/flowchartjs">Host on GitHub</a>
     </el-col>
   </el-row>
 </template>
